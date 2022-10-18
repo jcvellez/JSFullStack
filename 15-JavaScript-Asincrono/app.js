673475af4a2d267543fd6acd004af3b8b35a4c8b
jcvellez <jcvellez@live.com>
2022-10-18 00:09:23 -0500

    Me toca Seccion 15 clase 158 Promises

--- 15-JavaScript-Asincrono/app.js
+++ 15-JavaScript-Asincrono/app.js
@@ -36,45 +36,95 @@
 // getUsuarios();
 
 
-///Ejemplo del uso de promesas
-//Ejemplo 1
-let promesa = new Promise((resolve, reject) => {
-    if (false) {
-        resolve('Prueba exitosa');
-    } else {
-        reject('Prueba fallida');
-    }
+// De CallBacks a Promesas
+//metodo trae el arreglo completo
+let getUsuariosIDs = new Promise((response, reject) => {
+    setTimeout(() => {
+        response(['101', '102', '103']);
+    }, 1500);
 });
+//metodo devuleve un usuario
+let getUsuario = userID => {
+    return new Promise((resolve, reject) => {
+        setTimeout((id) => {
+            let usuario = {
+                email: 'jcvellez@gmail.com',
+                nombre: 'Juan Carlos'
+            }
+            resolve(`${id} - ${usuario.nombre} - ${usuario.email}`);
+        }, 1500, userID);
+    })
+};
+//metodo devuelve un permiso
+let getPermisos = id => {
+    return new Promise((resolve, reject) => {
+        setTimeout((id) => {
+            let permiso = ['admin', 'creador'];
+            resolve(permiso);
+        }, 1500, id);
+    })
+};
 
-promesa.then(response => {
-    console.log(response);
-}).catch(error => {
-    console.log(error);
-});
+
+//llamado principal
+getUsuariosIDs
+    .then(IDs => {
+        console.log(IDs);
+        return getUsuario(IDs[2]);
+    })
+    .then((usuario) => {
+        console.log(usuario);
+        return getPermisos(usuario.id);
+    })
+    .then((permisos)=>{
+        console.log(permisos);
+    })
+    .catch(() => {
+        console.log('Error');
+    });
 
 
-//Ejemplo 2
-let miPromesa = Promise.resolve('Comida');
-miPromesa.then(respuesta => console.log(respuesta));
+// ///Ejemplo del uso de promesas
+// //Ejemplo 1
+// let promesa = new Promise((resolve, reject) => {
+//     if (false) {
+//         resolve('Prueba exitosa');
+//     } else {
+//         reject('Prueba fallida');
+//     }
+// });
+
+// promesa.then(response => {
+//     console.log(response);
+// }).catch(error => {
+//     console.log(error);
+// });
 
 
-//Ejemplo3
-let miPromesa2 = new Promise((resolve, reject) => {
-    if (true) {
-        setTimeout(() => {
-            resolve(4);
-        }, 2000);
-    } else {
-        setTimeout(() => {
-            reject('Hubo un error');
-        }, 2000);
-    }
+// //Ejemplo 2
+// let miPromesa2a = Promise.resolve('Comida exitosa');
+// let miPromesa2b = Promise.reject('Comida fallida');
+// miPromesa2a.then(respuesta => console.log(respuesta));
+// miPromesa2b.catch(error => console.log(error));
 
-});
 
-miPromesa2.then((response) => {
-    response *= 2;
-    console.log(response);
-}).catch(error => {
-    console.log(error);
-});
+// //Ejemplo3
+// let miPromesa2 = new Promise((resolve, reject) => {
+//     if (true) {
+//         setTimeout(() => {
+//             resolve(4);
+//         }, 2000);
+//     } else {
+//         setTimeout(() => {
+//             reject('Hubo un error');
+//         }, 2000);
+//     }
+
+// });
+
+// miPromesa2.then( response => {
+//     response *= 2;
+//     console.log(response);
+// }).catch( error => {
+//     console.log(error);
+// });
